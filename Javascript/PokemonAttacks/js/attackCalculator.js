be152944a13cf575c5f0531645d20760e1f41a64
and951 <andresjimqui@gmail.com>
2017-07-04 13:23:28 -0600

    Finish pokemon training

--- Javascript/PokemonAttacks/js/attackCalculator.js
+++ Javascript/PokemonAttacks/js/attackCalculator.js
@@ -1,15 +1,19 @@
-var  calculateAttack = function(){
-  var typesDistribution = new pokemonType();
-  var attackType = document.getElementById('attackType').value;
-  var defenseType = document.getElementById('defenseType').value;
-  console.log(attackType + " "+defenseType);
-  console.log(100*typesDistribution.getEfectivness(attackType,defenseType));
-}
+var pokemons = {};
+
 var createPokemon= function(pSection){
-  var name =  document.getElementById('name'+pSection).value;
+  //For the api that we are using the pokemon name needs to be im lower case
+  var name =  document.getElementById('name'+pSection).value.toLowerCase();;
   var type = document.getElementById('type'+pSection).value;
   var attack = document.getElementById('attack'+pSection).value;
   var defense = document.getElementById('defense'+pSection).value;
-  var pokemonInstance = new pokemon(name,type,attack,defense);
-  console.log(JSON.stringify(pokemonInstance));
+  var pokemonInstance = new pokemon(name,type,attack,defense,pSection);
+  pokemons[pSection] = pokemonInstance;
+}
+
+var attack = function(pAttackPokemonSection,pDefensePokemonSection){
+  var typesDistribution = new pokemonType();
+  var attackPokemon = pokemons[pAttackPokemonSection];
+  var defensePokemon = pokemons[pDefensePokemonSection];
+  var damage = 50 * (attackPokemon.attack / defensePokemon.defense) * typesDistribution.getEfectivness(attackPokemon.type,defensePokemon.type);
+  alert(attackPokemon.name + " attacks "+defensePokemon.name+ " with a damage of "+damage);
 }
--- Javascript/PokemonAttacks/js/pokemon.js
+++ Javascript/PokemonAttacks/js/pokemon.js
@@ -1,28 +1,38 @@
 var pokemon = class {
-  constructor(pName, pType, pAttack, pDefense) {
+  constructor(pName, pType, pAttack, pDefense, pSection) {
     this.name = pName;
     this.type = pType;
     this.attack = pAttack;
     this.defense = pDefense;
     this.sprite = "";
+    this.section = pSection;
+    console.log(JSON.stringify("SECTION " + this.section));
     this.getExtraData();
 
   }
-  loadEnviornment(pPokemonId) {
-
+  loadEnviornment(actualPokemon) {
+    this.sprite = actualPokemon.sprites.front_default;
+    console.log(JSON.stringify("Attaching sprite to #pokemon" + this.section));
+    var section = this.section;
+    $("#pokemon" + section).attr("src", actualPokemon.sprites.front_default);
+    $('#pokemon' + section).hover(function() {
+      $('#pokemon'+ section).attr("src", actualPokemon.sprites.back_default)
+    }, function() {
+      $('#pokemon' + section).attr("src", actualPokemon.sprites.front_default)
+    });
   }
   getExtraData() {
     var pokeUrl = "http://pokeapi.co/api/v2/pokemon-form/" + this.name;
+    var thisClass = this;
+
     console.log(pokeUrl);
     $.ajax({
       url: pokeUrl,
       contentType: 'application/json',
       dataType: 'json',
       success: function(actualPokemon) {
-        console.log(JSON.stringify(actualPokemon));
-        this.sprite = actualPokemon.sprites.front_default;
-        $("#pokemon1").attr("src",actualPokemon.sprites.front_default);
-        $('#pokemon1').hover(function(){$('#pokemon1').attr("src",actualPokemon.sprites.back_default)},function(){$('#pokemon1').attr("src",actualPokemon.sprites.front_default)});
+        thisClass.loadEnviornment(actualPokemon);
+
       },
       error: function() {
         console.log("Error");
